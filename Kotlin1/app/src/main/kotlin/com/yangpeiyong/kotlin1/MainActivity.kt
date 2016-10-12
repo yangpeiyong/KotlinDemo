d9d33974c91afadca8208a7d7bc16a3807353f4c
yangpeiyong <yctx.yang@gmail.com>
2016-10-12 15:35:30 +0800

    add Kotlin1

--- Kotlin1/app/src/main/kotlin/com/yangpeiyong/kotlin1/MainActivity.kt
+++ Kotlin1/app/src/main/kotlin/com/yangpeiyong/kotlin1/MainActivity.kt
@@ -2,12 +2,30 @@ package com.yangpeiyong.kotlin1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+//import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        val name = "Kotlin"
+//        helloTxt.setText("Hello, $name")
+//    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        verticalLayout {
+            val name = editText(){
+                hint = "This is hint"
+            }
+            button("Say Hello") {
+                width = wrapContent
+                height = wrapContent
+                onClick { toast("Hello, ${name.text}!") }
+            }
+        }
     }
 }
